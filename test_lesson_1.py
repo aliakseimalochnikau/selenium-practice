16c281378a108306f982f2ca66da3e62733ce4de
Aliaksei Malochnikau <aliaksei.malochnikau@aholddelhaize.com>
2023-11-29 22:43:56 +0100

    Upload hometask for selenium lesson 1

---
+++ test_lesson_1.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestLessonOne:
+    @pytest.mark.parametrize("url, page_title", [("https://www.amazon.com/", "Amazon"),
+                                                 ("https://www.apple.com/", "Apple"),
+                                                 ("https://www.google.com/", "Google")])
+    def test_websites_chrome(self, driver_chrome, url, page_title):
+        driver_chrome.get(url)
+        assert page_title in driver_chrome.title
+        driver_chrome.save_screenshot(f"test_{page_title}.png")
