801950f0bbbdab9fe77bd88c2c021e7d3e7540ba
Aliaksei Malochnikau <aliaksei.malochnikau@aholddelhaize.com>
2023-11-30 16:23:02 +0100

    Add message for failed tests

--- test_lesson_1.py
+++ test_lesson_1.py
@@ -7,5 +7,5 @@ class TestLessonOne:
                                                  ("https://www.google.com/", "Google")])
     def test_websites_chrome(self, driver_chrome, url, page_title):
         driver_chrome.get(url)
-        assert page_title in driver_chrome.title
+        assert page_title in driver_chrome.title, f"Expected {page_title}, but was {driver_chrome.title}"
         driver_chrome.save_screenshot(f"test_{page_title}.png")
